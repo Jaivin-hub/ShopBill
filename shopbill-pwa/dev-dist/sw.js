04bdb14c2cf78eebf747e03a92fdf90618f0373a
sics-jaivin <jaivin.sics@gmail.com>
2025-10-06 23:13:07 +0530

    pwa icons updated

--- shopbill-pwa/dev-dist/sw.js
+++ shopbill-pwa/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c5f6b949'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ke806oht8kg"
+    "revision": "0.bjv03l2pago"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
